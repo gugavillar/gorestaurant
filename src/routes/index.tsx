8200c6882b867160f0928f7bb2b56192e2871a75
Guga Villar <grsv.21@gmail.com>
2021-07-28 15:47:51 -0300

    iniciando aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 
-import { Dashboard } from "../pages/Dashboard";
+import { Admin } from "../pages/Admin";
+import { Dashboard } from '../pages/Dashboard';
 
 export function Routes() {
    return (
       <Switch>
+         <Route path="/admin" component={Admin} />
          <Route path="/" exact component={Dashboard} />
+
       </Switch>
    );
 }
